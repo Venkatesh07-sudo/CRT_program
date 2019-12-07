d87f7e094d2ccd92b6d62c2cc77dd9da9a19ad43
Venkatesh07-sudo <58410662+Venkatesh07-sudo@users.noreply.github.com>
2019-12-07 11:11:34 +0530

    Update C_Programs.c

--- C_Programs.c
+++ C_Programs.c
@@ -179,5 +179,25 @@ int main()
         default :printf("invalid input");
     }
     }
+//////////////////////////
+#include <stdio.h>
 
+int main()
+{
+    int a,b;
+    char ch;
+    scanf("%d%d",&a,&b);
+    scanf(" %c",&ch);
+    switch(ch)
+    {
+        case '+':printf("%d",a+b);break;
+        case '-':printf("%d",a-b);break;
+        case '*':printf("%d",a*b);break;
+        case '/':printf("%d",a/b);break;
+        case '%':printf("%d",a%b);break;
+        
+        
+        default :printf("invalid input");
+    }
+    }
 
