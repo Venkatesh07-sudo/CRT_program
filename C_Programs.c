712087c3d1e9aa924f5087e3017a2313a35487b2
Venkatesh07-sudo <58410662+Venkatesh07-sudo@users.noreply.github.com>
2019-12-07 11:35:27 +0530

    Update C_Programs.c

--- C_Programs.c
+++ C_Programs.c
@@ -200,4 +200,18 @@ int main()
         default :printf("invalid input");
     }
     }
+///////////////////////////////
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    i=1;
+    do
+        {
+            printf("%d",i);
+    i++;
+        }
+        while(i<=10);
+}
 
